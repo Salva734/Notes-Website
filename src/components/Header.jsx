8aaebbfe3d09a962b1e0dd4bc37869d9ab36282c
Salva734 <salvador11504dev@gmail.com>
2025-08-16 23:18:33 -0600

    fixed a bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
-import {useState} from 'react'
-
 function Header() {
-  
+
   return (
     <header className="flex justify-between">
         <h1 className='font-bold text-4xl m-2'>Notas</h1>
-        <button onClick={handleClick} className="border-2 rounded-md m-2 p-2">+ Nueva nota</button>
+        <button className="border-2 rounded-md m-2 p-2">+ Nueva nota</button>
     </header>
   )
 }
