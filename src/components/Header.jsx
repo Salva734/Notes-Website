b6f2f9fbbc9190505eb8e0cfe552ba2f3ede5017
Salva734 <salvador11504dev@gmail.com>
2025-08-16 23:16:20 -0600

    removed some useless code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,7 @@
 import {useState} from 'react'
 
 function Header() {
-
-const [isHidden, setIsHidden] = useState(true);
-
-function handleClick(){
-  if(isHidden === false){
-    setIsHidden(true)
-  }
-  else{
-    setIsHidden(false)
-  }
-  console.log(isHidden)
-}
-
+  
   return (
     <header className="flex justify-between">
         <h1 className='font-bold text-4xl m-2'>Notas</h1>
